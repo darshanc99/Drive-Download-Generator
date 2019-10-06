bc8ad80bb96a88e740990191c0a376bac6e752b2
KaustubhDamania <kaustubh.damania@gmail.com>
2019-10-06 23:23:10 +0530

    Added CLI argument for Drive link

--- script.py
+++ script.py
@@ -1,4 +1,10 @@
-link = input("Enter the link:")
+import sys
+
+if len(sys.argv) < 2:
+    link = input("Enter the link: ")
+else:
+    link = sys.argv[1]
+
 baselink = "https://drive.google.com/uc?export=download&id="
 new = link.split('/')[5]
 new = baselink+new
