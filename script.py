310e564478e2d376a4c28d9342dee0775835ff00
KaustubhDamania <kaustubh.damania@gmail.com>
2019-10-07 00:30:32 +0530

    Added optional argument for direct download

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import sys
+import requests
+from math import ceil
 
 if len(sys.argv) < 2:
     link = input("Enter the link: ")
@@ -9,3 +11,19 @@ baselink = "https://drive.google.com/uc?export=download&id="
 new = link.split('/')[5]
 new = baselink+new
 print("New link:",new)
+
+if len(sys.argv) > 2 and sys.argv[-1] == '--download':
+    print("Downloading the file....", end="")
+    res = requests.get(new)
+    args = res.headers['Content-Disposition']
+    args = args.split(";")
+    filename = args[-2].split("\"")[-2]
+    print("named '{}'".format(filename))
+
+    download_file = open(filename,'wb')
+    download_size = int(res.headers['content-length'])
+
+    for chunk in res.iter_content(ceil(download_size/100)):
+        download_file.write(chunk)
+
+    print('Download successful!')
